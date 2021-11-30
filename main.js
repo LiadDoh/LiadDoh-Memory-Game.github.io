24ff9ed608396009958304156355bbbe4b8dff30
roeeavir <44678382+roeeavir@users.noreply.github.com>
2021-11-30 15:56:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-function getDetails() {
+function setAndGetDetails() {
+    // Load Details
+    var url_string = window.location.href;
+    var url = new URL(url_string);
+    var player1 = url.searchParams.get("player1");
+    var player2 = url.searchParams.get("player2");
+    var boardsize = url.searchParams.get("game_board_size");
+    console.log(boardsize);
+    const gameData = {
+        player1: {
+            name: player1,
+            score: 0,
+            gamesWon: 0
+        },
+        player2: {
+            name: player2,
+            score: 0,
+            gamesWon: 0
+        },
+        game_board_size: boardsize
+
+    }
+    gameData.player1.name = player1;
+    gameData.player2.name = player2;
+
     document.getElementById("player1").innerHTML = gameData.player1.name;
     document.getElementById("player2").innerHTML = gameData.player2.name;
     document.getElementById("score1").innerHTML = "Score:" + gameData.player1.score;
@@ -7,7 +31,7 @@ function getDetails() {
     document.getElementById("gamesWon2").innerHTML = "Games Won:" + gameData.player2.gamesWon;
 }
 
-function createCard(gameContainer,i) {
+function createCard(gameContainer, i) {
     const cardDiv = document.createElement('div');
     const backDiv = document.createElement('div');
     const frontDiv = document.createElement('div');
@@ -30,15 +54,15 @@ function createCard(gameContainer,i) {
     gameContainer.appendChild(cardDiv);
 }
 
-function createBoard(cards,boardsize) {
+function createBoard(cards, boardsize) {
     let gameContainer = document.getElementById("yese").getElementsByClassName('game-container')[0];
 
     for (let i = 0; i < cards.length; i++) {
-        if (i == cards.length -1 && boardsize == 5) 
-            createCard(gameContainer,i);
+        if (i == cards.length - 1 && boardsize == 5)
+            createCard(gameContainer, i);
         else
             for (let j = 0; j < 2; j++) {
-                createCard(gameContainer,i);
+                createCard(gameContainer, i);
             }
     }
 }
@@ -50,16 +74,14 @@ function ready(boardsize) {
     let cardsArray = [];
     let coin = new Coin();
     let gameContainer = document.getElementById("yese").getElementsByClassName('game-container')[0];
-    if (boardsize == 4){
+    if (boardsize == 4) {
         cardsArray = [0, 1, 2, 3, 4, 5, 6, 7];
         gameContainer.classList.add("game-container-4");
-    }
-    else if(boardsize == 5)
-    {
+    } else if (boardsize == 5) {
         cardsArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
         gameContainer.classList.add("game-container-5");
     }
-    createBoard(cardsArray,boardsize);
+    createBoard(cardsArray, boardsize);
     let cards = Array.from(document.getElementsByClassName('card'));
     let game = new Game(cards);
     coin.flipBtn.addEventListener("click", () => {
@@ -67,31 +89,7 @@ function ready(boardsize) {
     }, 3000);
 }
 
-// Load Details
-var url_string = window.location.href;
-var url = new URL(url_string);
-var player1 = url.searchParams.get("player1");
-var player2 = url.searchParams.get("player2");
-var boardsize = url.searchParams.get("game_board_size");
-console.log(boardsize);
-const gameData = {
-    player1: {
-        name: player1,
-        score: 0,
-        gamesWon: 0
-    },
-    player2: {
-        name: player2,
-        score: 0,
-        gamesWon: 0
-    },
-    game_board_size: boardsize
-
-}
-gameData.player1.name = player1;
-gameData.player2.name = player2;
-
-window.onload = getDetails();
+window.onload = setAndGetDetails();
 
 if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', ready(boardsize));
